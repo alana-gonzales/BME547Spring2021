d6e197014f03b5c2bb52693c6ea00f8301382540
Alana Gonzales <alana.gonzales@duke.edu>
2021-02-12 16:40:22 -0500

    fixed typo: wrong variable used

--- test_linear_interpolator.py
+++ test_linear_interpolator.py
@@ -4,5 +4,4 @@ def test_return_y_value():
     from linear_interpolator import return_y_value
     result = return_y_value((1, 2,), (3, 4), 5)
     expected = 6
-    assert answer == expected
-
+    assert result == expected
