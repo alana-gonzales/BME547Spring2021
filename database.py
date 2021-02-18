7a3a48e473d21c6ee841deee4e188ed9674b325e
Alana Gonzales <alana.gonzales@duke.edu>
2021-02-18 16:25:26 -0500

    convert patient data from list to dictionary

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 def create_database_entry(name, id_no, age):
-    new_patient = [name, id_no, age, []]
+    # new_patient = [name, id_no, age, []]
+    new_patient = {"name": name, "id": id_no, "age": age, "test": list()}
 
     return new_patient
 
@@ -19,13 +20,37 @@ def find_patient(id_no, db):
 
 def add_test_result(id_no, db, test_name, test_result):
     for patient in db:
-        if patient[1] == id_no:
-            patient[3].append((test_name, test_result))
+        if patient["id"] == id_no:
+            patient["test"].append((test_name, test_result))
     # don't need to return anything because it is a mutable list, so the list is updated everywhere
+
+def print_db(db):
+    for patient in db:
+        print_patient(patient)
+
+def print_patient(patient):
+    for key in patient:
+        print("  {} = {}".format(key, patient[key]))
+
+def demo(patient):
+    if "name" in patient:
+        print("Name is {}".format(patient["name"]))
+    
+    if "DOB" in patient:
+        print("DOB is {}".format(patient["DOB"]))
+
+def minor_or_adult(patient):
+    if "age" in patient:
+        if patient["age"] < 18:
+            return "minor"
+        else:
+            return "adult"
+    else:
+        return None 
 
 def main():
     db = list()
-    db.append(create_database_entry("Ann Ables", 1, 30))
+    db.append(create_database_entry("Ann Ables", 1, 17))
     db.append(create_database_entry("Bob Boyles", 2, 31))
     db.append(create_database_entry("Chris Chou", 3, 32))
     db.append(create_database_entry("David Dinkins", 4, 33))
@@ -43,11 +68,18 @@ def main():
     #my_name = find_patient(2, db)
     #print(my_name)
 
-    print(db)
+    print_db(db)
     add_test_result(3, db, "HDL", 65)
-    print(db)
+    print_db(db)
     add_test_result(3, db, "LDL", 80)
-    print(db)
+    print_db(db)
+
+    demo(db[0])
+
+    if minor_or_adult(db[0]) == "minor":
+        print("minor")
+    else:
+        print("adult")
 
 if __name__ == "__main__":
     main()
